c994707e7c922bd31e37b66e26092f6869ed7690
olivia-leach <olivia.i.m.leach@gmail.com>
2019-02-10 22:13:18 -0500

    setting up deploy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <Router>
         <div className="App mdc-typography main-body">
           <Header />
-          <Route path="/" exact component={Landing} />
+          <Route path="/" component={Landing} />
           {/*<Route path="/tour/" component={Tour} />*/}
           {/*<Route path="/store/" component={Store} />*/}
           {/*<Route path="/contact/" component={Contact} />*/}
