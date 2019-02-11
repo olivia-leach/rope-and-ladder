2ef732d73c4fa7c62035611ca027ad706313b8a8
olivia-leach <olivia.i.m.leach@gmail.com>
2019-02-10 22:09:41 -0500

    Updates

---
+++ precache-manifest.44af822aae82daa29138fca76135faa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82cea58d5c38ac2ddedf",
+    "url": "/rope-and-ladder/static/css/main.d3eb683c.chunk.css"
+  },
+  {
+    "revision": "82cea58d5c38ac2ddedf",
+    "url": "/rope-and-ladder/static/js/main.82cea58d.chunk.js"
+  },
+  {
+    "revision": "897c4e1c589f6aa49077",
+    "url": "/rope-and-ladder/static/js/1.897c4e1c.chunk.js"
+  },
+  {
+    "revision": "f8d1646865c2d5a135ff",
+    "url": "/rope-and-ladder/static/js/runtime~main.f8d16468.js"
+  },
+  {
+    "revision": "50d6f292ef4e4dcfec049b097cb467c8",
+    "url": "/rope-and-ladder/static/media/Cover-Border.50d6f292.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rope-and-ladder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a2207b63fa68cd60761f290b8740e33",
+    "url": "/rope-and-ladder/index.html"
+  }
+];
