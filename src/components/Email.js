594dc95376d929d9dbbeda7e92640b938abb08ec
olivia-leach <olivia.i.m.leach@gmail.com>
2019-12-08 21:30:24 -0500

    mail chimpify

--- src/components/Email.js
+++ src/components/Email.js
@@ -7,8 +7,9 @@ class Email extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            email: '',
-            name: '',
+            EMAIL: '',
+            FNAME: '',
+            LNAME: '',
         }
     }
 
@@ -28,7 +29,16 @@ class Email extends Component {
     }
 
     getFormData() {
-        const data = { email: this.state.email, name: this.state.name }
+        const data = {
+            EMAIL: this.state.EMAIL,
+            FNAME: this.state.FNAME,
+            LNAME: this.state.LNAME
+            // status: 'subscribed',
+            // merge_fields: {
+                // FNAME: this.state.name,
+                // LNAME: this.state.name
+            // }
+        }
         data.formDataNameOrder = JSON.stringify(data);
         return data;
     }
@@ -38,15 +48,17 @@ class Email extends Component {
         this.setState({ submitting: true })
         const data = this.getFormData();
     
-        let encoded = Object.keys(data).map(function(k) {
-            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-        }).join('&')
+        // let encoded = Object.keys(data).map(function(k) {
+        //     return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+        // }).join('&')
 
-        const url = 'https://script.google.com/macros/s/AKfycbx0hWsJBWiSzDOIAzJldsRg0wdj7NwwtFO6vFNDS_cqbKAbvtQe/exec'
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.send(encoded);
+        // const url = 'https://script.google.com/macros/s/AKfycbx0hWsJBWiSzDOIAzJldsRg0wdj7NwwtFO6vFNDS_cqbKAbvtQe/exec'
+        // const listId = '9e67587f52'
+        const url = 'https://ropeandladder.us4.list-manage.com/subscribe/post-json?u=929e4d360b1bc0291a85bc367&amp;id=51f155eaca&amp;c=?'
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', url)
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8')
+        xhr.send(data)
         xhr.onreadystatechange = function() {
             console.log(xhr.responseText)
             if (xhr.status === 200) {
@@ -61,43 +73,70 @@ class Email extends Component {
         // url encode form data for sending as post data
     }
 
+
     render() {
-        const emailValid = this.state.email.includes('@') && this.state.email.includes('.')
-        const canSubmit = !!this.state.name && emailValid
+        const emailValid = this.state.EMAIL.includes('@') && this.state.EMAIL.includes('.')
+        const canSubmit = !!this.state.FNAME && !!this.state.LNAME && emailValid
         return (
         <div className='email-container'>
             {this.state.submitted || this.state.submitting ? <h1 className='call-to-action-instructions text-center'>Thanks. We'll be in touch soon.</h1> :
             <Fragment>
-            <div className='flex flex-center'>
-                <img className='sign-up' src={Signup} />
-            </div>
-            <div className='flex flex-center input-container'>
-            <div>
-                <div className="mdc-text-field">
-                <input type="text" name='name' id="name-field" className="mdc-text-field__input" onChange={this.onTextChange} />
-                <label className="mdc-floating-label" htmlFor="name-field">
-                    NAME
-                </label>
-                <div className="mdc-line-ripple" />
+                <div className='flex flex-center'>
+                    <img className='sign-up' src={Signup} alt='Sign Up To Receive Album Release Updates' />
                 </div>
-            </div>
-            <div>
-                <div className="mdc-text-field">
-                <input type="email" name='email' id="email-field" className="mdc-text-field__input" onChange={this.onTextChange} />
-                <label className="mdc-floating-label" htmlFor="email-field">
-                    EMAIL
-                </label>
-                <div className="mdc-line-ripple" />
+                <div id="mc_embed_signup">
+                    <form action="https://ropeandladder.us4.list-manage.com/subscribe/post?u=929e4d360b1bc0291a85bc367&amp;id=51f155eaca" target="_blank" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="flex flex-center input-container" novalidate>
+                        <div className="mdc-text-field">
+                            <input type="text" value={this.state.FNAME} name="FNAME" className="mdc-text-field__input"  id="mce-FNAME" onChange={this.onTextChange} />
+                            <label className="mdc-floating-label" htmlFor="mce-FNAME">First Name </label>
+                            <div className="mdc-line-ripple" />
+                        </div>
+                        <div className="mdc-text-field">
+                            <input type="text" value={this.state.LNAME} name="LNAME" className="mdc-text-field__input"  id="mce-LNAME" onChange={this.onTextChange} />
+                            <label className="mdc-floating-label" htmlFor="mce-LNAME">Last Name </label>
+                            <div className="mdc-line-ripple" />
+                        </div>
+                        <div className="mdc-text-field">
+                            <input type="email" value={this.state.EMAIL} name="EMAIL" className="mdc-text-field__input" id="mce-EMAIL" onChange={this.onTextChange} />
+                            <label className="mdc-floating-label" htmlFor="mce-EMAIL">Email Address </label>
+                            <div className="mdc-line-ripple" />
+                        </div>
+                        <div id="mce-responses" className="clear">
+                            <div className="response" id="mce-error-response" style={{display: 'none'}}></div>
+                            <div className="response" id="mce-success-response" style={{display: 'none'}}></div>
+                        </div>
+                        <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true"><input type="text" name="b_929e4d360b1bc0291a85bc367_51f155eaca" tabindex="-1" value="" /></div>
+                        <input disabled={!canSubmit} type='submit' value={this.state.submitting ? 'Signing up...' : 'Subscribe'} name="subscribe" id="mc-embedded-subscribe" className="mdc-button mdc-button--raised subscribe" />
+                    </form>
                 </div>
-            </div>
-            <button className="mdc-button mdc-button--raised subscribe" disabled={!canSubmit || this.state.submitting || this.state.submitted} onClick={this.submitForm}>
-                {this.state.submitting ?
-                    <span className="mdc-button__label">Signing up...</span>
-                :
-                <span className="mdc-button__label">
-                    {this.state.submitted ? 'Thank you' : 'Subscribe'}</span>}
-            </button>
-            </div>
+
+
+                    {/*<div>
+                        <div className="mdc-text-field">
+                        <input type="text" name='name' id="name-field" className="mdc-text-field__input" onChange={this.onTextChange} />
+                        <label className="mdc-floating-label" htmlFor="name-field">
+                            NAME
+                        </label>
+                        <div className="mdc-line-ripple" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="mdc-text-field">
+                        <input type="email" name='email' id="email-field" className="mdc-text-field__input" onChange={this.onTextChange} />
+                        <label className="mdc-floating-label" htmlFor="email-field">
+                            EMAIL
+                        </label>
+                        <div className="mdc-line-ripple" />
+                        </div>
+                    </div>
+                    <button className="mdc-button mdc-button--raised subscribe" disabled={!canSubmit || this.state.submitting || this.state.submitted} onClick={this.submitForm}>
+                        {this.state.submitting ?
+                            <span className="mdc-button__label">Signing up...</span>
+                        :
+                        <span className="mdc-button__label">
+                            {this.state.submitted ? 'Thank you' : 'Subscribe'}</span>}
+                    </button>
+                        </div>*/}
             </Fragment>}
             </div>
         )
