dcd2ee258215bfb32e2928936301f131b3bc0957
Olivia Clark <olivia@circulation.com>
2020-11-03 22:28:56 -0500

    fix: mailchimp integration

--- src/components/Email/Email.tsx
+++ src/components/Email/Email.tsx
@@ -8,107 +8,85 @@ const Email = () => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [honeyPot, setHoneyPot] = useState('')
-  const [isSubmitted, setIsSubmitted] = useState(false)
-  const [isSubmitting, setIsSubmitting] = useState(false)
 
   const onEmailChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value)
-    setIsSubmitted(false)
   }, [])
 
   const onFirstNameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setFirstName(e.target.value)
-    setIsSubmitted(false)
   }, [])
 
   const onLastNameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setLastName(e.target.value)
-    setIsSubmitted(false)
   }, [])
 
   const onHoneyPotChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setHoneyPot(e.target.value)
   }, [])
 
-    const submitForm = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-      setIsSubmitting(true)
-      const data: { EMAIL: string, FNAME: string, LNAME: string, formDataNameOrder?: string } = {
-        EMAIL: email,
-        FNAME: firstName,
-        LNAME: lastName,
-    }
-    data.formDataNameOrder = JSON.stringify(data)
+  const emailValid = email.includes('@') && email.includes('.')
+  const canSubmit = !!firstName && !!lastName && emailValid && !honeyPot
 
-      const url = 'https://ropeandladder.us4.list-manage.com/subscribe/post-json?u=929e4d360b1bc0291a85bc367&amp;id=51f155eaca&amp;c=?'
-      const xhr = new XMLHttpRequest()
-      xhr.open('GET', url)
-      xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8')
-      xhr.send(JSON.stringify(data))
-      xhr.onreadystatechange = function() {
-          console.log(xhr.responseText)
-          if (xhr.status === 200) {
-            setIsSubmitting(false)
-            setIsSubmitted(true)
-          }
-          return;
-      }
-    }, [email, firstName, lastName])
-
-    const emailValid = email.includes('@') && email.includes('.')
-    const canSubmit = !!firstName && !!lastName && emailValid && !honeyPot
-
-    return (
-      <div className='email-container dark-section'>
-      {isSubmitted || isSubmitting ? <h1 className='call-to-action-instructions text-center'>Thanks. We'll be in touch soon.</h1> :
-        <>
-          <div className='sign-up-text'>
-            <p>SUBSCRIBE TO THE ROPE AND LADDER MAILING LIST</p>
-          </div>
-          <form action="https://ropeandladder.us4.list-manage.com/subscribe/post?u=929e4d360b1bc0291a85bc367&amp;id=51f155eaca" target="_blank" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="flex flex-center input-container" noValidate onSubmit={submitForm}>
-            <TextField
-              color='secondary'
-              label='First Name'
-              margin='dense'
-              onChange={onFirstNameChange}
-              size='small'
-              value={firstName}
-              variant='filled'
-            />
-            <TextField
-              color='secondary'
-              label='Last Name'
-              margin='dense'
-              onChange={onLastNameChange}
-              size='small'
-              value={lastName}
-              variant='filled'
-            />
-            <TextField
-              color='secondary'
-              label='Email Address'
-              margin='dense'
-              onChange={onEmailChange}
-              size='small'
-              value={email}
-              variant='filled'
-            />
-            <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
-              <input type="text" name="b_929e4d360b1bc0291a85bc367_51f155eaca" tabIndex={-1} value={honeyPot} onChange={onHoneyPotChange} />
-            </div>
-            <Button
-              color='secondary'
-              disabled={!canSubmit}
-              variant='contained'
-              size='large'
-              type='submit'
-            >
-              {isSubmitting ? 'Signing up...' : 'Subscribe'}
-            </Button>
-          </form>
-        </>}
+  return (
+    <div className='email-container dark-section'>
+      <div className='sign-up-text'>
+        <p>SUBSCRIBE TO THE ROPE AND LADDER MAILING LIST</p>
       </div>
-    )
+      <form
+        action="https://ropeandladder.us4.list-manage.com/subscribe/post?u=929e4d360b1bc0291a85bc367&amp;id=51f155eaca"
+        className="flex flex-center input-container"
+        target="_blank"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        noValidate
+      >
+        <TextField
+          color='secondary'
+          label='First Name'
+          margin='dense'
+          name='FNAME'
+          onChange={onFirstNameChange}
+          size='small'
+          value={firstName}
+          variant='filled'
+        />
+        <TextField
+          color='secondary'
+          label='Last Name'
+          margin='dense'
+          name='LNAME'
+          onChange={onLastNameChange}
+          size='small'
+          value={lastName}
+          variant='filled'
+        />
+        <TextField
+          color='secondary'
+          label='Email Address'
+          margin='dense'
+          name='EMAIL'
+          onChange={onEmailChange}
+          size='small'
+          value={email}
+          variant='filled'
+        />
+        <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+          <input type="text" name="b_929e4d360b1bc0291a85bc367_51f155eaca" tabIndex={-1} value={honeyPot} onChange={onHoneyPotChange} />
+        </div>
+        <Button
+          color='secondary'
+          disabled={!canSubmit}
+          variant='contained'
+          size='large'
+          type='submit'
+        >
+          Subscribe
+        </Button>
+      </form>
+    </div>
+  )
 }
 
 export default Email
